5db6e58d6ac0058209d5fa76debdf73608df564f
Kirils Golubevs <kirils.golubevs@wandoofinance.com>
2019-03-12 23:19:08 +0200

    Improve accessability

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,33 @@ import { Route, Link } from "react-router-dom";
 import ProductIdForm from "./components/ProductIdForm";
 
 const App: React.FunctionComponent = () => (
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/faq">Users</Link>
-        </li>
-      </ul>
-    </nav>
+  <>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/faq">Users</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
 
-    <Route path="/" exact component={ProductIdForm} />
-    <Route path="/about" component={() => <div>About Page</div>} />
-    <Route path="/faq" component={() => <div>FAQ Page</div>} />
-  </div>
+    <main>
+      <Route path="/" exact component={ProductIdForm} />
+      <Route path="/about" component={() => <div>About Page</div>} />
+      <Route path="/faq" component={() => <div>FAQ Page</div>} />
+    </main>
+
+    <footer>
+      Footer <section />
+    </footer>
+  </>
 );
 
 export default App;
