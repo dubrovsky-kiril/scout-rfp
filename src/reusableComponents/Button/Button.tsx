832d1f9926f2c20ebe2af9c15d78b74f7af575c7
Kirils Golubevs <kirils.golubevs@wandoofinance.com>
2019-03-12 23:27:58 +0200

    Improve accessability 2

--- src/reusableComponents/Button/Button.tsx
+++ src/reusableComponents/Button/Button.tsx
@@ -1,16 +1,4 @@
 import * as React from "react";
-import styled from "styled-components";
-
-// just an example of styled-components use
-const StyledButton = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-  cursor: pointer;
-`;
 
 type ButtonTypes = {
   /** defines type of button (submit, button etc.) */
@@ -30,9 +18,9 @@ const Button: React.FunctionComponent<ButtonTypes> = ({
   isPending = false
 }) => {
   return (
-    <StyledButton type={type} form={form}>
+    <button type={type} form={form}>
       {isPending ? "Loading" : txt}
-    </StyledButton>
+    </button>
   );
 };
 
